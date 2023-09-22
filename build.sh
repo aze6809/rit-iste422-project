815597b316cf3902029226241e2f583437cb9ab2
aze6809 <71939491+aze6809@users.noreply.github.com>
2023-09-21 23:53:55 -0400

    Update build.sh
    Changed build.sh to try and update classpaths

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ echo "Cleaning existing classes..."
 rm -f *.class
 # This command looks for matching files and runs the rm command for each file
 # Note that {} are replaced with each file name
-find . -name \*.class -exec rm {} \;
+find ./build -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/main/java/*.java
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore src/main/test/java/EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
